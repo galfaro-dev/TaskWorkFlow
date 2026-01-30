8543fe0149771b3900af881f2f5220d01c4ac256
Yoshino1996 <greivinalfaro76@gmail.com>
2026-01-30 09:23:10 -0800

    Domain was create

--- TaskWorkFlow.Domain/Entities/TaskItem.cs
+++ TaskWorkFlow.Domain/Entities/TaskItem.cs
@@ -1,31 +1,65 @@
-﻿using TaskWorkFlow.Domain.Enums;
+﻿namespace TaskWorkFlow.Domain.Entities;
 
-namespace TaskWorkFlow.Domain.Entities;
+using TaskWorkFlow.Domain.Enums;
 
 public class TaskItem
 {
     public Guid Id { get; private set; }
-
     public string Title { get; private set; }
-
     public string Description { get; private set; }
 
-    public TaskState Status { get; private set; }
+    public TaskState State { get; private set; }
 
     public Guid? AssignedUserId { get; private set; }
-
     public DateTime CreatedAt { get; private set; }
-
     public DateTime? UpdatedAt { get; private set; }
 
-    private TaskItem() { } // requerido para EF (más adelante)
+    private TaskItem() { }
 
     public TaskItem(string title, string description)
     {
         Id = Guid.NewGuid();
         Title = title;
         Description = description;
-        Status = TaskState.Pending;
+        State = TaskState.Pending;
         CreatedAt = DateTime.UtcNow;
     }
+
+    // ===== COMPORTAMIENTO DEL DOMINIO =====
+
+    public void Start()
+    {
+        if (State != TaskState.Pending)
+            throw new InvalidOperationException("Only pending tasks can be started.");
+
+        State = TaskState.InProgress;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void Complete()
+    {
+        if (State != TaskState.InProgress)
+            throw new InvalidOperationException("Only in-progress tasks can be completed.");
+
+        State = TaskState.Completed;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void Block()
+    {
+        if (State == TaskState.Completed)
+            throw new InvalidOperationException("Completed tasks cannot be blocked.");
+
+        State = TaskState.Blocked;
+        UpdatedAt = DateTime.UtcNow;
+    }
+    public void Unblock()
+    {
+        if (State != TaskState.Blocked)
+            throw new InvalidOperationException("Only blocked tasks can be unblocked.");
+
+        State = TaskState.InProgress;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
 }
