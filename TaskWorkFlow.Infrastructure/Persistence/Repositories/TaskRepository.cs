a50ff52692a47d1365083b82a4cfc32252ccec8d
Yoshino1996 <greivinalfaro76@gmail.com>
2026-02-06 10:59:03 -0800

    Add infraestructure project. it has the repository and the conecction with the db. Right now i´m working in memory but in the future this project will have a datebase.

---
+++ TaskWorkFlow.Infrastructure/Persistence/Repositories/TaskRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskWorkFlow.Application.Interfaces.Persistence;
+using TaskWorkFlow.Domain.Entities;
+
+namespace TaskWorkFlow.Infrastructure.Persistence.Repositories;
+
+public class TaskRepository : ITaskRepository
+{
+    private readonly TaskDbContext _context;
+
+    public TaskRepository(TaskDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<TaskItem?> GetByIdAsync(Guid id)
+    {
+        return await _context.Tasks.FirstOrDefaultAsync(t => t.Id == id);
+    }
+
+    public async Task AddAsync(TaskItem task)
+    {
+        _context.Tasks.Add(task);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(TaskItem task)
+    {
+        _context.Tasks.Update(task);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ TaskWorkFlow.Infrastructure/Persistence/TaskDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskWorkFlow.Domain.Entities;
+
+namespace TaskWorkFlow.Infrastructure.Persistence;
+
+public class TaskDbContext : DbContext
+{
+    public TaskDbContext(DbContextOptions<TaskDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TaskItem> Tasks => Set<TaskItem>();
+}
